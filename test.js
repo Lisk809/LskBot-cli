c7649b8103222edd41842bcf0d385ee3dda8772f
lisk809 <Liutsian_Lisk@126.com>
2023-01-04 16:02:21 +0800

    上传

---
+++ test.js
@@ -0,0 +1,7 @@
+process.on('SIGINT', function () {
+	             console.log('✘   已退出LskBot-cli');
+	             process.exit();
+	        });
+var a=setTimeout(()=>{
+	console.log("aaaaaa")
+},3000)
